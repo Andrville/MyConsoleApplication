f7a1c70c3ca791b78a80e39d6a1274c9af7c1698
Andrville <andk@simcorp.com>
2024-11-06 19:03:18 +0200

    First

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! and everyone in room");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloFromMyfirstAPP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        
+        }
+    }
+}
